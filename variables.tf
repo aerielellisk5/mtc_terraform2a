308d044093d45da04310bfd87db699036946c23e
Aeriel <aerielellisk5@gmail.com>
2023-01-07 20:03:31 +0000

    refactoring the terraform deployment

--- variables.tf
+++ variables.tf
@@ -1,8 +1,29 @@
+
+# variable "env" {
+#   type = string
+#   default = "dev"
+#   description = "Env to deploy to"
+# }
+
+variable "image" {
+  type = map
+  description = "image for container"
+  default = {
+    dev = "nodered/node-red:latest"
+    prod = "nodered/node-red:latest-minimal"
+  }
+}
+
 variable "ext_port" {
-  type = number
+  type = map
   
   validation {
-    condition = var.ext_port <= 65535 && var.ext_port > 0
+    condition = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
+    error_message = "The external port must be in the valid port range 0-65535."
+  }
+  
+  validation {
+    condition = max(var.ext_port["prod"]...) < 1980 && min(var.ext_port["prod"]...) >= 1880
     error_message = "The external port must be in the valid port range 0-65535."
   }
 }
@@ -17,7 +38,7 @@ variable "int_port" {
   }
 }
 
-variable "container_count" {
-  type = number
-  default = 1
+locals {
+  container_count = length(var.ext_port[terraform.workspace])
 }
+
